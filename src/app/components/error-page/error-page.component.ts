00bad0a6fd0af91cc8d599645695382a47dbd80c
novakand <a79264799999@gmail.com>
2026-06-18 06:10:29 +0300

    fix

--- src/app/components/error-page/error-page.component.ts
+++ src/app/components/error-page/error-page.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AnimatedContainer } from '../animatedcontainer';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/components/menu.component';
 @Component({
   selector: 'error-page',
   templateUrl: './error-page.component.html',
@@ -10,7 +14,12 @@ import { FooterComponent } from '../footer/footer.component';
   imports: [
     CommonModule,
     AnimatedContainer,
-    FooterComponent
+    TranslateModule,
+    ButtonModule,
+    RouterLink,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent
 
   ],
   standalone: true
