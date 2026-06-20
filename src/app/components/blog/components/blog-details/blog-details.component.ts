48f0c721a39ee7599fff19c65a6678d2c9d4d9ee
novakand <a79264799999@gmail.com>
2026-06-20 20:49:50 +0300

     fix

--- src/app/components/blog/components/blog-details/blog-details.component.ts
+++ src/app/components/blog/components/blog-details/blog-details.component.ts
@@ -4,11 +4,18 @@ import { HeaderComponent } from "../../../header/header.component";
 import { CommonModule } from "@angular/common";
 import { AnimatedContainer } from "../../../animatedcontainer";
 import { MenuComponent } from "../../../menu/components/menu.component";
+import { TagModule } from 'primeng/tag';
 @Component({
     selector: "app-blog-details",
     standalone: true,
-    imports: [CommonModule, AnimatedContainer, MenuComponent,
-        HeaderComponent, FooterComponent],
+    imports: [
+        CommonModule,
+        AnimatedContainer,
+        MenuComponent,
+        HeaderComponent,
+        FooterComponent,
+        TagModule
+    ],
     templateUrl: './blog-details.component.html',
     styleUrl: './blog-details.component.scss',
 })
@@ -16,4 +23,10 @@ export class BlogDetaislComponent {
 
     public title = 'Blog Details';
     public description = 'Blog Details2';
+
+    public tags: string[] = [
+        'Travel',
+        'Destination',
+        'Tour Guide'
+    ];
 }
